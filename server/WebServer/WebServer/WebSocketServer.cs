13da76c683dd5c4a7a8ab0e688b4b654acf65c27
ginolu <gino.lu@ohmplus.com>
2025-05-28 14:52:30 +0800

    123

--- server/WebServer/WebServer/WebSocketServer.cs
+++ server/WebServer/WebServer/WebSocketServer.cs
@@ -16,7 +16,7 @@ namespace WebServer
       listener.Prefixes.Add(uri);
       listener.Start();
       Console.WriteLine("WebSocket server started.");
-//1234564
+//12345
       while (true)
       {
         HttpListenerContext context = await listener.GetContextAsync();
